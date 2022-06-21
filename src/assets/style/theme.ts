84a6a462db8b56498f92b2f8143fee772cfeeda2
Michal Molenda <michalus88@gmail.com>
2022-06-21 17:41:06 +0200

    created theme with colors nad font size

---
+++ src/assets/style/theme.ts
@@ -0,0 +1,17 @@
+export const theme = {
+  colors: {
+    white: "#FFFFFF",
+    lightGrey: "#F7F8FA",
+    green: "darkgreen",
+    black: "#111111",
+    success: "#8FCB81",
+    error: "#FF8383",
+  },
+  fontSize: {
+    xxl: "34px",
+    xl: "24px",
+    l: "17px",
+    m: "12px",
+    s: "11px",
+  },
+};
