8feb0cf6fe6fa6d564beddabfafd31e8093b8cc4
Michal Molenda <michalus88@gmail.com>
2022-06-30 23:03:52 +0200

    added group export to hooks and views

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Navigation } from "./components/navigation/Navigation";
-import { Register } from "./views/register/Register";
-import { Home } from "./views/home/Home";
+import { Home, Register, Login, Game } from "./views";
 import { ErrorMessage } from "./components/error-message/ErrorMessage";
 import { useError } from "./hooks/useError";
 import { MainTemplate } from "./components/templates/main-template";
@@ -18,6 +17,7 @@ function App() {
       <Navigation logOut={signOut} user={user}></Navigation>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/game" element={<Game />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
---
+++ src/hooks/index.ts
@@ -0,0 +1,3 @@
+export * from "./useAuth";
+export * from "./useCheckRes";
+export * from "./useError";
