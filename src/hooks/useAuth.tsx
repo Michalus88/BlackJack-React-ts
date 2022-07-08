3ca8a1fc4ab97a999610198262085071dd4895e3
Michal Molenda <michalus88@gmail.com>
2022-07-09 01:18:07 +0200

    implemented isErrResMsg and handled 401 res.status

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect, useState, createContext } from "react";
 import { RegisterReq, LoggedUserRes } from "types";
-import { useError } from "./useError";
-import { useCheckRes } from "./useCheckRes";
+import { useError } from "./";
 import { useNavigate } from "react-router";
+import { isResErrorMsg } from "../helpers/isErrorMsg";
 
 interface AuthContextType {
   user: LoggedUserRes | null;
@@ -13,10 +13,11 @@ interface AuthContextType {
 const AuthContext = createContext<AuthContextType>(null!);
 
 export const AuthProvider = ({ children }: { children: JSX.Element }) => {
-  const [user, setUser] = useState<LoggedUserRes | null>(null);
-  const { dispatchError } = useError();
-  const isResError = useCheckRes();
   const navigate = useNavigate();
+  const { dispatchError, error } = useError();
+  const [user, setUser] = useState<LoggedUserRes | null>(null);
+  const [wasPrevMsgUnauthorized, setWasPrevMsgUnauthorized] =
+    useState<boolean>(false);
 
   useEffect(() => {
     (async () => {
@@ -25,17 +26,26 @@ export const AuthProvider = ({ children }: { children: JSX.Element }) => {
           credentials: "include",
         });
         if (res.ok) {
-          const resBody = (await res.json()) as LoggedUserRes;
-          setUser(resBody);
+          const user = (await res.json()) as LoggedUserRes;
+          setUser(user);
         } else {
           setUser(null);
         }
-      } catch (e) {
-        console.log(e);
+      } catch (err) {
         dispatchError("Server is anavailable.");
       }
     })();
   }, []);
+
+  useEffect(() => {
+    if (wasPrevMsgUnauthorized) {
+      setWasPrevMsgUnauthorized(false);
+      signOut();
+    }
+    if (error === "Unauthorized") {
+      setWasPrevMsgUnauthorized(true);
+    }
+  }, [error]);
 
   const signIn = async (data: RegisterReq) => {
     try {
@@ -48,10 +58,13 @@ export const AuthProvider = ({ children }: { children: JSX.Element }) => {
         },
         body: JSON.stringify(data),
       });
-      const user = (await res.json()) as LoggedUserRes;
-      if (isResError(res)) return;
-      setUser(user);
-      navigate("/");
+      if (await isResErrorMsg(res)) {
+        dispatchError("wrong login or password");
+      } else {
+        const user = (await res.json()) as LoggedUserRes;
+        setUser(user);
+        navigate("/");
+      }
     } catch (error) {
       dispatchError();
     }
@@ -60,13 +73,23 @@ export const AuthProvider = ({ children }: { children: JSX.Element }) => {
   const signOut = async () => {
     try {
       const res = await fetch("http://localhost:3001/api/auth/logout", {
+        method: "POST",
         credentials: "include",
       });
-
-      if (isResError(res)) return;
-      setUser(null);
+      const errMsg = await isResErrorMsg(res);
+      if (res.status === 401) {
+        setUser(null);
+      } else {
+        if (errMsg) {
+          dispatchError(errMsg);
+        } else {
+          setUser(null);
+        }
+      }
     } catch (error) {
       dispatchError();
+    } finally {
+      navigate("/login");
     }
   };
 
