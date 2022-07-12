16ecd2613e4d11e59252490aa5f2e2bede3fcc2d
Michal Molenda <michalus88@gmail.com>
2022-07-12 02:56:39 +0200

    implemented notification for game

--- src/views/game/Game.tsx
+++ src/views/game/Game.tsx
@@ -1,13 +1,18 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNotification, useGameFetch } from "../../hooks/";
 import { Dashboard } from "./dashboard/Dashboard";
 import { Table } from "./table/Table";
 import { Wrapper } from "./game.styled";
 import { NotificationMode } from "../../components/notification/Notification";
+import { LoadingIndicator } from "../../components/loadin-indicator/loading-ndicator";
+import { Wrapper } from "./game.styled";
+
+import { GameResult, NO_MEANS } from "./constants";
 
 export const Game = () => {
-  const { callApi, player } = useGameFetch();
-  const { dispatchNotification } = useNotification();
+  const { callApi, player, isLoading } = useGameFetch();
+  const { dispatchNotification, message, mode } = useNotification();
+  const [wasPrevMsg, setWasPrevMsg] = useState<boolean>(false);
 
   useEffect(() => {
     callApi();
@@ -16,21 +21,54 @@ export const Game = () => {
   useEffect(() => {
     let timeOutId: NodeJS.Timeout;
     if (player?.means === 0 && player?.playerBet === 0) {
-      dispatchNotification(null, NotificationMode.WARMING);
-    } else {
-      (async () => {
-        if (player?.gameResult) {
+      dispatchNotification(NotificationMode.WARMING, NO_MEANS);
+      return;
+    }
+
+    if (player?.gameResult) {
+      console.log(player.gameResult);
+      switch (player.gameResult) {
+        case 1:
+          dispatchNotification(NotificationMode.WARMING, GameResult.LOOSE);
+          break;
+        case 2:
+          dispatchNotification(NotificationMode.INFO, GameResult.DROW);
+          break;
+        case 3:
+          dispatchNotification(NotificationMode.SUCCESS, GameResult.WIN);
+          break;
+        default:
+          dispatchNotification(
+            NotificationMode.ERROR,
+            "Given game result status don't egsist"
+          );
+          break;
+      }
+    }
+  }, [player]);
+
+  useEffect(() => {
+    let timeOutId: NodeJS.Timeout;
+    if (message === NO_MEANS) return;
+    if (mode !== NotificationMode.ERROR) {
+      if (wasPrevMsg) {
+        setWasPrevMsg(false);
+        (async () => {
           timeOutId = setTimeout(async () => {
             await callApi();
-          }, 2000);
-        }
-      })();
+          }, 1000);
+          return;
+        })();
+      }
+      if (message) {
+        setWasPrevMsg(true);
+      }
     }
 
     return () => {
       clearTimeout(timeOutId);
     };
-  }, [player]);
+  }, [message]);
 
   return (
     <Wrapper>
