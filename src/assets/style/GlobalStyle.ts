c996ad3d2237209facdff00bd262bead58cf45c4
Michal Molenda <michalus88@gmail.com>
2022-07-12 22:50:29 +0200

    set overflow hidden for body

--- src/assets/style/GlobalStyle.ts
+++ src/assets/style/GlobalStyle.ts
@@ -14,7 +14,7 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Bitter', sans-serif;
     margin: 0;
     padding: 0;
-    overflow-y: hidden;
+    overflow: hidden;
     background-color:black ;
     background-image: url(${img.symbols});
   background-position: center;
