013d838c70e3d4dc89f2e2b9c0d7f5c9987eedfc
Michal Molenda <michalus88@gmail.com>
2022-07-14 11:57:34 +0200

    defined break points for device resolution

---
+++ src/assets/style/breakPoints.ts
@@ -0,0 +1,13 @@
+export const size = {
+  xs: "375px",
+  s: "476px",
+  sm: "675px",
+  md: "778px",
+};
+
+export const device = {
+  xs: `(max-width: ${size.xs})`,
+  s: `(max-width: ${size.s})`,
+  sm: `(max-width: ${size.sm})`,
+  md: `(max-width: ${size.md})`,
+};
