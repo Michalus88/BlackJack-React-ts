4b7c9535e9eb4328fdca0def366370474564c018
Michal Molenda <michalus88@gmail.com>
2022-07-14 12:05:06 +0200

    chenged icone and animation

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,6 +1,13 @@
 import React, { FC, useState } from "react";
 import { LoggedUserRes } from "types";
-import { Icone, MobileLink, LinkWrapper, NavWrapper } from "./navbar.style";
+import {
+  Line1,
+  Line2,
+  Line3,
+  Hamburger,
+  LinkWrapper,
+  NavWrapper,
+} from "./navbar.style";
 import { StyledLink } from "../styled-link/styledLink";
 interface Props {
   user: LoggedUserRes | null;
@@ -9,15 +16,14 @@ interface Props {
 
 export const Navbar: FC<Props> = ({ user, logOut }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
     <NavWrapper>
-      <Icone onClick={() => setIsOpen((curr) => !curr)}>
-        {isOpen ? (
-          <i className="fas fa-times"></i>
-        ) : (
-          <i className={"fas fa-bars"}></i>
-        )}
-      </Icone>
+      <Hamburger onClick={() => setIsOpen((prev) => !prev)}>
+        <Line1 isOpen={isOpen}></Line1>
+        <Line2 isOpen={isOpen}></Line2>
+        <Line3 isOpen={isOpen}></Line3>
+      </Hamburger>
       <LinkWrapper isOpen={isOpen}>
         <StyledLink to="/">Home</StyledLink>
         {user ? (
@@ -30,7 +36,7 @@ export const Navbar: FC<Props> = ({ user, logOut }) => {
             Logout
           </StyledLink>
         ) : (
-          <MobileLink to="/login">Login</MobileLink>
+          <StyledLink to="/login">Login</StyledLink>
         )}
       </LinkWrapper>
     </NavWrapper>
--- src/components/navbar/navbar.style.ts
+++ src/components/navbar/navbar.style.ts
@@ -1,46 +1,65 @@
 import styled from "styled-components";
-import { NavLink } from "react-router-dom";
+import { device, size } from "../../assets/style/breakPoints";
 
 export const NavWrapper = styled.div`
   display: none;
   position: absolute;
   right: 0;
   top: 0;
-`;
-
-export const Icone = styled.div`
-  position: absolute;
-  top: 0px;
-  left: -40px;
-  font-size: 40px;
-  line-height: 50px;
-  transition: 0.3s linear;
-  color: ${({ theme }) => theme.colors.green};
-  :hover {
-    cursor: pointer;
-  }
-`;
-
-export const MobileLink = styled(NavLink)`
-  border: ${({ theme }) => theme.colors.green} solid 1px;
-  padding: 10px 25px;
-  cursor: pointer;
-  font-weight: bold;
-  text-decoration: none;
-  color: ${({ theme }) => theme.colors.green};
-  background-color: ${({ theme }) => theme.colors.black};
-  text-align: center;
-  &:hover {
-    box-shadow: 1px -1px 15px 2px ${({ theme }) => theme.colors.lightgreen};
-  }
-  &.active {
-    background-color: ${({ theme }) => theme.colors.lightgreen};
-    box-shadow: 1px -1px 15px 1px ${({ theme }) => theme.colors.lightgreen};
+  z-index: 999;
+  @media (max-width: ${size.md}), (max-height: ${size.sm}) {
+    display: block;
   }
 `;
 
 export const LinkWrapper = styled.div<{ isOpen: boolean }>`
-  display: ${({ isOpen }) => (!isOpen ? "none" : "flex")};
-  position: relative;
+  display: flex;
+  background-color: ${({ theme }) => theme.colors.transparentBlack};
+  width: 100vw;
+  display: flex;
+  justify-content: space-around;
+  transition: transform 0.5s ease-in-out;
+  transform: ${({ isOpen }) =>
+    !isOpen ? "translateX(100%)" : "translate(0%)"};
+  @media ${device.s} {
+    justify-content: flex-start;
+  }
+`;
+
+export const Hamburger = styled.div`
+  width: 35px;
+  height: 29px;
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  display: flex;
   flex-direction: column;
+  justify-content: space-between;
+  z-index: 2;
+`;
+
+export const Line = styled.span`
+  width: 100%;
+  height: 4px;
+  display: block;
+  border-radius: 10px;
+  background: ${({ theme }) => theme.colors.green};
+  @media (max-height: 530px) and (orientation: landscape) {
+    background: ${({ theme }) => theme.colors.green};
+    box-shadow: 1px 2px 25px 2px ${({ theme }) => theme.colors.lightgreen};
+  }
+`;
+export const Line1 = styled(Line)<{ isOpen: boolean }>`
+  transform-origin: 0% 0%;
+  transition: transform 0.4s ease-in-out;
+  transform: ${({ isOpen }) => (isOpen ? "rotate(45deg)" : "none")};
+`;
+export const Line2 = styled(Line)<{ isOpen: boolean }>`
+  transition: transform 0.2s ease-in-out;
+  transform: ${({ isOpen }) => (isOpen ? "scaleY(0)" : "none")};
+`;
+export const Line3 = styled(Line)<{ isOpen: boolean }>`
+  transform-origin: 0% 100%;
+  transition: transform 0.4s ease-in-out;
+  transform: ${({ isOpen }) => (isOpen ? "rotate(-45deg)" : "none")};
 `;
